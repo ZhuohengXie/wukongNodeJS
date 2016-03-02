011489242f9dec54f392823ac9e7cb41992a91ae
ZhuohengXie <zhxie@Zhuohengs-MacBook-Pro.local>
2016-03-02 11:36:23 -0500

    update

--- nodejs_beginner/index.js
+++ nodejs_beginner/index.js
@@ -2,6 +2,10 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
+
+
+
+
 var handle = {};
 
 handle["/hello"] = requestHandlers.hello;
@@ -9,4 +13,3 @@ handle["/world"] = requestHandlers.world;
 handle["/"] = requestHandlers.homepage;
 
 server.start(router.route, handle);
-
